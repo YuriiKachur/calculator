f08df4735598c32057e71636bb4737d0257ed341
Yurii Kachur <163346915+YuriiKachur@users.noreply.github.com>
2024-11-13 21:18:58 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, render_template
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hello, Flask is running!"
+    return render_template("index.html")  # Рендеринг HTML-файлу з інтерфейсом
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
